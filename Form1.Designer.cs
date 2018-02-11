a2da79ff48debede2954e8c9e032f118d9fd0d63
dezidaisy <32540506+dezidaisy@users.noreply.github.com>
2018-02-12 00:06:58 +0530

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(359, 91);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(903, 220);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(350, 91);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(86, 385);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(359, 83);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "OK";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Location = new System.Drawing.Point(86, 590);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1098, 469);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Source Folder";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(681, 250);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 32);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Target Folder";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 529);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Files Copied";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(80, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(496, 78);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Copy Directory";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1304, 1197);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(0, 0, 50, 100);
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public static string sourcePath;
+        public static string targetPath;
+ 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void RFileCopy(string sourcePath, string targetPath)
+        {
+            string fileName;
+            string destFile;
+
+            if (!System.IO.Directory.Exists(targetPath))
+            {
+                //MessageBox.Show("Target Directory does not exist", "Error");
+
+            }
+            else
+            {
+
+                if (System.IO.Directory.Exists(sourcePath))
+                {
+                    string[] files = System.IO.Directory.GetFiles(sourcePath);
+
+                    // Copy the files 
+                    foreach (string s in files)
+                    {
+
+                        fileName = System.IO.Path.GetFileName(s);
+                        destFile = System.IO.Path.Combine(targetPath, fileName);
+                        if (!System.IO.File.Exists(destFile))
+                        {
+                            System.IO.File.Copy(s, destFile, false);
+                            listBox1.Items.Add(s);
+                            listBox1.Update();
+                            listBox1.Refresh();
+
+
+                            Application.DoEvents();
+                        }
+                        else
+                        {
+                           // MessageBox.Show("File " + destFile + " already exists!", "Error");
+                        }
+
+
+                    }
+                }
+                else
+                {
+                    //MessageBox.Show("Source path does not exist!", "Error");
+                }
+            }
+
+            string[] subDirs = null;
+            if (sourcePath != null)
+            {
+                subDirs = System.IO.Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories);
+            }
+            // Recursive call for each subdirectory.
+            string fileName2;
+            if (subDirs != null)
+            {
+                foreach (string s in subDirs)
+                {
+                    fileName2 = s.Remove(0, sourcePath.Length + 1);
+                    string sourceFolder = System.IO.Path.Combine(sourcePath, fileName2);
+                    string destFolder = System.IO.Path.Combine(targetPath, fileName2);
+
+                    if (!System.IO.Directory.Exists(destFolder))
+                    {
+                        System.IO.Directory.CreateDirectory(destFolder);
+
+                    }
+                    RFileCopy(sourceFolder, destFolder);
+
+
+                }
+            }
+            
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    sourcePath = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    targetPath = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            RFileCopy(sourcePath,targetPath);
+            listBox1.Items.Add("***Done***");
+            listBox1.Update();
+            listBox1.Refresh();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
